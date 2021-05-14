fbf5514115c2dad5aaf034a2213f7f99db1d57ec
khagerman <71734063+khagerman@users.noreply.github.com>
2021-05-14 10:55:24 -0500

    first commit

---
+++ models.py
@@ -0,0 +1,44 @@
+"""Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+GENERIC_IMAGE = "https://tinyurl.com/demo-cupcake"
+
+
+class Cupcake(db.Model):
+    """Cupcake Model"""
+
+    __tablename__ = "cupcake"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    flavor = db.Column(
+        db.Text,
+        nullable=False,
+    )
+    size = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    rating = db.Column(db.Float(), nullable=False)
+    image = db.Column(
+        db.Text,
+    )
+
+    def image_url(self):
+        """Return image for pet -- bespoke or generic."""
+
+        return self.image or GENERIC_IMAGE
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
